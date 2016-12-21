03cdb221c5ac700e15e1032b2db8cd14860e2098
naim18-meet <naimjm@gmail.com>
2016-12-21 19:34:59 +0200

    I was stupid at the tiem

--- student.py
+++ student.py
@@ -16,7 +16,7 @@ class Student:
         print(str(self.name) + ' is from ' + str(self.hometown) + ' and is ' + str(self.age) + '. He/she is ' + str(self.height) + 'cm tall and their favorite ice cream flavor ' + str(self.fav_IC) + '.')
 
     def get_giraffe_gap(self):
-        X= 500-(int(self.age))
+        X= 500-(int(self.height))
         return X
 
     def get_days_to_birthday(self):
